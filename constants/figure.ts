b6127d91c5cb9b5adeafde3e4727991c65c293c3
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-04-18 22:49:51 +0300

    Add: NullFigure entity

--- constants/figure.ts
+++ constants/figure.ts
@@ -1,1 +1,8 @@
+import { FigureType } from "@/types/figure";
+
 export const FIGURE_AVATAR_PATH = "/figures/";
+
+/**
+ * A figure to fill empty tiles on the field
+ */
+export const NullFigure: FigureType = {type: null, color: null};
